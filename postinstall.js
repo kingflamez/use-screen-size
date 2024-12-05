daa42f3b63f13c91c8defb2518906d315b5e8201
kingflamez <flamekeed@gmail.com>
2024-12-05 07:40:58 +0100

    add post install message

---
+++ postinstall.js
@@ -0,0 +1,12 @@
+// ANSI escape codes for styling
+const reset = '\x1b[0m';
+const bright = '\x1b[1m';
+const fgGreen = '\x1b[32m';
+const fgCyan = '\x1b[36m';
+const fgMagenta = '\x1b[35m';
+
+console.log(`
+${fgGreen}${bright}Thank you for installing use-screen-size!${reset}
+${fgCyan}If you find this package helpful, please consider starring our GitHub repository:${reset}
+${fgMagenta}https://github.com/kingflamez/use-screen-size${reset}
+`);
